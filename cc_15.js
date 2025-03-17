e9ed507a1e9280421862bd0861e999fb7ca098dd
Henok <henokassefa494@gmail.com>
2025-03-16 23:42:56 -0400

    Task 1 - Base Structure Setup.

---
+++ cc_15.js
@@ -0,0 +1,7 @@
+// Task 1
+// Select the riskDashboard container
+const riskDashboard = document.getElementById("riskDashboard");
+
+// Print message to console
+console.log("Risk Dashboard Loaded");
+
